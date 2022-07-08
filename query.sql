8002becaab26a22a12fc025b2880c1c17c4be199
Simone Sada <simone.sada@gmail.com>
2022-07-08 14:50:38 +0200

    Query su singola tabella completate (con bonus)

--- query.sql
+++ query.sql
@@ -1,6 +1,6 @@
 -- ------ Query su singola tabella
--- 
--- ```
+
+
 -- 1- Selezionare tutte le software house americane (3)
 SELECT *
 FROM software_houses
@@ -27,22 +27,47 @@ FROM tournaments
 WHERE year = '2015';
 
 -- 6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
--- 
+SELECT *
+FROM awards
+WHERE description LIKE '%facere%';
+
 -- 7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
--- 
+SELECT videogame_id
+FROM category_videogame
+WHERE category_id = '2'
+OR category_id = '6'
+GROUP BY videogame_id;
+
 -- 8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
--- 
+SELECT *
+FROM reviews
+WHERE rating >= '2'
+AND rating <= '4';
+
 -- 9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
--- 
--- 10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
--- 
+SELECT *
+FROM videogames
+WHERE YEAR(release_date) = '2020';
+
+-- 10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da 5 stelle, mostrandoli una sola volta (443)
+SELECT videogame_id
+FROM reviews
+WHERE rating = '5'
+GROUP BY videogame_id;
+
 -- *********** BONUS ***********
 -- 
 -- 11- Selezionare il numero e la media delle recensioni per il videogioco con ID = 412 (review number = 12, avg_rating = 3)
--- 
+SELECT COUNT(*) AS 'review_number', AVG(avg_rating) AS 'Media'
+FROM reviews
+WHERE videogame_id = '412';
+
 -- 12- Selezionare il numero di videogame che la software house con ID = 1 ha rilasciato nel 2018 (13)
--- ```
--- 
+SELECT COUNT(*)
+FROM videogames
+WHERE software_house_id = '1'
+AND YEAR(release_date) = '2018';
+
 -- ------ Query con group by
 -- 
 -- ```
